f39fc508dde34f98c49326e42a5a3e70a02abec8
julian <julian.harley@gmail.com>
2021-03-09 17:33:58 +0000

    add github action and remove secret key

--- segment_sandbox/settings.py
+++ segment_sandbox/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'eqg5049o)7j!64^9%zpp+u5c$sx$q$89ir67&y@m=e$glilw9n'
+SECRET_KEY = os.env('SECRET_KEY','notverysecretkey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
