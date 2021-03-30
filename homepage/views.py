a8670b221bcd14669cfdda7ecdd5b370d728c91c
julian <julian.harley@gmail.com>
2021-03-30 11:16:40 +0100

    reformat this

--- homepage/views.py
+++ homepage/views.py
@@ -3,12 +3,21 @@ from django.shortcuts import render
 
 def home(request):
 
-    a = 1 + 2 + 4
+
+
+
+    a=1+2+4
 
     #  some things
     #  that should be
     #  reformatted!
 
-    print(1 + 2 + 4 + 4 + 45 + 444)
+    print(
+        1 + 2 + 4 +
+        4 + 45 + 444 * 888 * 88
+        *222
+        *3
+
+    );
 
     return render(request=request, template_name="homepage/home.html")
