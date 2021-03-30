a70b4ce51cfec30b632e470d262e9c28a9d7af57
julian <julian.harley@gmail.com>
2021-03-30 10:10:51 +0100

    reformattable commit

--- homepage/views.py
+++ homepage/views.py
@@ -3,5 +3,19 @@ from django.shortcuts import render
 
 def home(request):
 
-    a = 1 + 2 + 4
+
+
+
+    a=1+2+4
+
+    #  some things
+    #  that should be
+    #  reformatted!
+
+    print(
+        1 + 2 + 4 +
+        4 + 45 + 444
+
+    );
+
     return render(request=request, template_name="homepage/home.html")
